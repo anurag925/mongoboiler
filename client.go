19ac8d16fc207cfb948f267c2828df40540824e0
Anurag Upadhyay <anurag.upadhyay@kuvera.in>
2023-01-29 14:26:20 +0530

    added disconnect in db

--- client.go
+++ client.go
@@ -19,6 +19,10 @@ func New(client *mongo.Client, name string, ctx context.Context) *DB {
 	return &DB{client.Database(name), client, ctx}
 }
 
+func (db DB) Disconnect() {
+	db.client.Disconnect(db.ctx)
+}
+
 // Collection is the wrapper for Mongo Collection
 type Collection struct {
 	*DB
